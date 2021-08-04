0b6c0a56c7d2853ca9a8402937780743533e4ee6
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2021-08-04 10:18:19 -0300

    Set modal config

--- src/app/shared/components/alerta/alerta.component.ts
+++ src/app/shared/components/alerta/alerta.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Alerta } from '../../models/alerta';
 
 @Component({
   selector: 'dio-alerta',
@@ -8,26 +9,30 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class AlertaComponent implements OnInit {
 
-  titulo = "Sucesso!";
-  descricao = "Seu registro foi cadastrado com sucesso!";
-  btnSucesso = "OK";
-  btnCancelar = "Cancelar";
-  corBtn = "primary";
-  possuirBtnFechar = false;
+  alerta: Alerta = {
+    titulo: "Sucesso!",
+    descricao: "Seu registro foi cadastrado com sucesso!",
+    btnSucesso: "OK",
+    btnCancelar: "Cancelar",
+    corBtnSucesso: "accent",
+    corBtnCancelar: "warn",
+    possuirBtnFechar: false,
+  }
 
   constructor(
     public dialogRef: MatDialogRef<AlertaComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: Alerta
   ) { }
 
   ngOnInit() {
     if(this.data) {
-      this.titulo = this.data.titulo || this.titulo;
-      this.descricao = this.data.titulo || this.descricao;
-      this.btnSucesso = this.data.titulo || this.btnSucesso;
-      this.btnCancelar= this.data.titulo || this.btnCancelar;
-      this.corBtn = this.data.titulo || this.corBtn;
-      this.possuirBtnFechar = this.data.titulo || this.possuirBtnFechar;
+      this.alerta.titulo = this.data.titulo || this.alerta.titulo;
+      this.alerta.descricao = this.data.descricao || this.alerta.descricao;
+      this.alerta.btnSucesso = this.data.btnSucesso || this.alerta.btnSucesso;
+      this.alerta.btnCancelar= this.data.btnCancelar || this.alerta.btnCancelar;
+      this.alerta.corBtnSucesso = this.data.corBtnSucesso || this.alerta.corBtnSucesso;
+      this.alerta.corBtnCancelar = this.data.corBtnCancelar || this.alerta.corBtnCancelar;
+      this.alerta.possuirBtnFechar = this.data.possuirBtnFechar || this.alerta.possuirBtnFechar;
     }
   }
 
